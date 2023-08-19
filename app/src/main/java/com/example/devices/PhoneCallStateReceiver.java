51f7bb2f4d8e2c9d05e28ce172643f12b04def39
duynam236 <daonam230699@gmail.com>
2023-08-19 14:48:22 +0700

    android telephony api

---
+++ app/src/main/java/com/example/devices/PhoneCallStateReceiver.java
@@ -0,0 +1,19 @@
+package com.example.devices;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class PhoneCallStateReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String phoneState = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        if (phoneState.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+            String phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            Toast.makeText(context.getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
